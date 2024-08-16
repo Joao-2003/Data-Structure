8bd98faacc7bd96e41cd12f9f11fca0fa148ee8c
Joao-2003 <100796909+Joao-2003@users.noreply.github.com>
2024-08-16 11:01:29 -0300

    Add files via upload

---
+++ CircularDoubleLinkedList/src/CircularDoubleLinkedList.java
@@ -0,0 +1,161 @@
+public class CircularDoubleLinkedList {
+    public Node sentinel;
+    public int size;
+
+    public CircularDoubleLinkedList() {
+        this.sentinel = new Node(Integer.MIN_VALUE);
+        this.sentinel.next = this.sentinel;
+        this.sentinel.previous = this.sentinel;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+
+    public int size() {
+        return this.size;
+    }
+
+    public void addToHead(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.sentinel.next;
+        newNode.previous = this.sentinel;
+        this.sentinel.next.previous = newNode;
+        this.sentinel.next = newNode;
+        if (this.size == 0) {
+            this.sentinel.previous = newNode;
+        }
+        this.size++;
+    }
+
+    public void addToTail(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.sentinel;
+        newNode.previous = this.sentinel.previous;
+        this.sentinel.previous.next = newNode;
+        this.sentinel.previous = newNode;
+        if (this.size == 0) {
+            this.sentinel.next = newNode;
+        }
+        this.size++;
+    }
+
+    public void removeFromHead() {
+        if (!isEmpty()) {
+            Node first = this.sentinel.next;
+            this.sentinel.next = first.next;
+            first.next.previous = this.sentinel;
+            this.size--;
+            if (this.size == 0) {
+                this.sentinel.previous = this.sentinel;
+            }
+        }
+    }
+
+    public void removeFromTail() {
+        if (!isEmpty()) {
+            Node last = this.sentinel.previous;
+            this.sentinel.previous = last.previous;
+            last.previous.next = this.sentinel;
+            this.size--;
+            if (this.size == 0) {
+                this.sentinel.next = this.sentinel;
+            }
+        }
+    }
+
+    public void delete(int index) {
+        if (!isEmpty() && index >= 0 && index < this.size) {
+            Node temp = this.sentinel.next;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            temp.previous.next = temp.next;
+            temp.next.previous = temp.previous;
+            this.size--;
+        }
+    }
+
+
+
+    public void insert(int index, int data) {
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (index == 0) {
+            addToHead(data);
+        } else if (index == this.size) {
+            addToTail(data);
+        } else {
+            Node newNode = new Node(data);
+            Node temp = this.sentinel.next;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp;
+            newNode.previous = temp.previous;
+            temp.previous.next = newNode;
+            temp.previous = newNode;
+            this.size++;
+        }
+    }
+
+    public void insert(int data){
+        Node newNode = new Node(data);
+        if (this.sentinel.next.data - data <= this.sentinel.previous.data - data){
+            Node aux = this.sentinel.next;
+            while (data > aux.next.data){
+                aux = aux.next;
+            }
+            newNode.next = aux.next;
+            newNode.previous = aux;
+            aux.next.previous = newNode;
+            aux.next = newNode;
+            this.size++;
+        } else {
+            Node aux = this.sentinel.previous;
+            while (data < aux.previous.data){
+                aux = aux.previous;
+            }
+            newNode.previous = aux.previous;
+            newNode.next = aux;
+            aux.previous.next = newNode;
+            aux.previous = newNode;
+            this.size++;
+        }
+    }
+
+    public void printIncreasing() {
+        Node node = this.sentinel.next;
+        while (node != this.sentinel) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public void printDecreasing() {
+        Node node = this.sentinel.previous;
+        while (node != this.sentinel) {
+            System.out.print(node.data + " ");
+            node = node.previous;
+        }
+        System.out.println();
+    }
+
+    public boolean search(int data) {
+        Node node = this.sentinel.next;
+        while (node != this.sentinel) {
+            if (data == node.data) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+}
+
---
+++ CircularDoubleLinkedList/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] vector = new int[1000];
+        Random random = new Random();
+        ArrayList<Integer> removed = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            vector[i] = random.nextInt(-9999,9999);
+        }
+        System.out.println(Arrays.toString(vector));
+
+        CircularDoubleLinkedList list = new CircularDoubleLinkedList();
+
+        list.addToHead(vector[0]);
+        for (int i = 1; i < 1000; i++) {
+            if (vector[i] < list.sentinel.next.data){
+                list.addToHead(vector[i]);
+            } else if (vector[i] > list.sentinel.previous.data){
+                list.addToTail(vector[i]);
+            } else {
+                list.insert(vector[i]);
+            }
+        }
+
+        list.printIncreasing();
+        list.printDecreasing();
+        System.out.println(list.size());
+
+        Node node = list.sentinel.next;
+        for (int i = 0; i < 1000; i++) {
+            if (isPrime(node.data)){
+                removed.add(node.data);
+                list.delete(i);
+            }
+            node = node.next;
+        }
+        list.printIncreasing();
+        System.out.println(list.size());
+        System.out.println(removed);
+        System.out.println(removed.size());
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
